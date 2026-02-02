3fa1bc3f8a629eee5b51ba250e801bc068295310
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-02-02 17:25:36 +0100

    It's ok

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express()
 
 app.set("view engine", "ejs");
-app.use(express.static("asset"));
+app.use(express.static("assets"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
