a8b1ed04b19a821d7921e2e0eee51aa8ca842794
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-02-02 16:52:21 +0100

    Add application myfunnyvalentine

---
+++ app.js
@@ -0,0 +1,33 @@
+// app.js // 
+
+import express from "express"
+
+const app = express()
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+   res.render("question");
+})
+
+app.get("/question", (req, res)=> {
+    res.render("question");
+})
+
+app.get("/yes", (req, res)=> {
+    res.render("yes"); 
+})
+
+app.post("/yes", (req, res)=> {
+    res.render("yes");
+})
+
+
+
+app.listen(3000, function () {
+  console.log("Server listening on port 3000")
+})
+
+
