0c160b35fae5b8873cee8abee144d340a4ac6bdc
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-02-02 17:19:38 +0100

    Change public to asset

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express()
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("asset"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
